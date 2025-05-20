33a752a420e50fcc61a60c716fbecbf6d7b23510
Aviancaswat <aviancaswat@gmail.com>
2025-05-20 09:00:01 -0500

    ALF1-1573 v2

--- tests/ALF1-1573/alf1-1573.spec.ts
+++ tests/ALF1-1573/alf1-1573.spec.ts
@@ -43,8 +43,8 @@ type copysType = {
 const copys: copysType = {
     idioma: 'es' as Lang,
     pais: 'CO',
-    fecha_salida: 'may 14',
-    fecha_llegada: 'may 20',
+    fecha_salida: 'may 24',
+    fecha_llegada: 'may 30',
     ciudad_origen: 'BAQ',
     ciudad_destino: 'BOG',
     es: {
@@ -115,7 +115,6 @@ test.describe('Comenzo prueba avianca', () => {
         });
 
         await page.goto('https://www.avianca.com/');
-        await takeScreenshot('01-goto-avianca');
 
         const consentBtn = page.locator('#onetrust-pc-btn-handler');
 
@@ -131,33 +130,24 @@ test.describe('Comenzo prueba avianca', () => {
         await origen.fill(copys['ciudad_origen']);
         await origen.press('Enter');
         await (page.locator('id=' + copys['ciudad_origen'])).click()
-        await takeScreenshot('03-ciudad-origen');
 
         const destino = page.getByPlaceholder(copys[idioma].destino);
         await destino.click();
         await destino.fill(copys['ciudad_destino']);
         await destino.press('Enter');
         await (page.locator('id=' + copys['ciudad_destino'])).click();
-        await takeScreenshot('04-ciudad-destino');
 
         const fechaIda = await page.locator('id=departureInputDatePickerId')
         fechaIda.click();
         await page.locator('span').filter({ hasText: copys['fecha_salida'] }).click();
-        await takeScreenshot('05-fecha-ida');
 
         await page.locator('span').filter({ hasText: copys['fecha_llegada'] }).click();
-        await takeScreenshot('06-fecha-vuelta');
 
-        await page.getByRole('button', { name: '' }).nth(1).click();
-        await page.getByRole('button', { name: '' }).nth(2).click();
-        await page.getByRole('button', { name: '' }).nth(3).click();
         const confirmar = await page.locator('div#paxControlSearchId > div > div:nth-of-type(2) > div > div > button')
         confirmar.click();
 
-        await takeScreenshot('07-seleccion-pasajeros');
         await expect(page.getByRole('button', { name: copys[idioma].buscar, exact: true })).toBeVisible()
         await page.getByRole('button', { name: copys[idioma].buscar, exact: true }).click();
-        await takeScreenshot('08-buscar');
 
         await page.waitForSelector('#pageWrap');
         await expect(page.locator(".journey_price_fare-select_label-text").first()).toBeVisible();
@@ -165,6 +155,195 @@ test.describe('Comenzo prueba avianca', () => {
         await page.waitForSelector(".journey_fares");
         await page.locator('.journey_fares').first().locator('.light-basic.cro-new-basic-button').click();
         //  await page.locator('.journey_fares').first().locator('.fare-flex').click();
-        await takeScreenshot('09-seleccion-vuelo-ida');
+
+        await page.waitForTimeout(1500);
+        const isVisibleModal = await page.locator("#FB310").first().isVisible();
+
+        if (isVisibleModal) {
+            await expect(page.locator(".cro-button.cro-no-accept-upsell-button")).toBeVisible();
+            await page.locator(".cro-button.cro-no-accept-upsell-button").first().click();
+        }
+
+        await page.waitForSelector("#journeysContainerId_1", { timeout: 15000 });
+        const containerVuelta = page.locator("#journeysContainerId_1");
+        await expect(containerVuelta).toBeVisible();
+        // await expect(page.locator('.journey_price_fare-select_label-text').nth(22)).toBeVisible();
+        await containerVuelta.locator(".journey_price_fare-select_label-text").first().click();
+        await containerVuelta.locator('.journey_fares').first().locator('.light-basic.cro-new-basic-button').click();
+        await page.waitForTimeout(1500);
+
+        const isVisibleModal2 = await page.locator("#FB310").first().isVisible();
+
+        if (isVisibleModal2) {
+            await expect(page.locator(".cro-button.cro-no-accept-upsell-button")).toBeVisible();
+            await page.locator(".cro-button.cro-no-accept-upsell-button").first().click();
+        }
+
+        await page.waitForSelector(".trip-summary");
+        const buttonConfirmResumen = page.locator(".button.page_button.btn-action");
+        await expect(buttonConfirmResumen).toBeVisible();
+        buttonConfirmResumen.scrollIntoViewIfNeeded();
+        await buttonConfirmResumen.click();
+
+        //página de pasajeros
+        await page.waitForSelector(".passenger_data_group");
+
+        await page.evaluate(() => {
+            const userNamesData: Array<string> = [
+                "john doe",
+                "jane smith",
+                "alexander wilson",
+                "maria gomez",
+                "roberto perez",
+                "lucia martinez",
+                "david hernandez",
+                "carla jones",
+                "luis vega",
+                "susan brown"
+            ];
+
+            const lastNamesData: Array<string> = [
+                "Doe",
+                "Smith",
+                "Wilson",
+                "Gomez",
+                "Perez",
+                "Martinez",
+                "Hernandez",
+                "Jones",
+                "Vega",
+                "Brown"
+            ];
+
+            const emailsData: Array<string> = [
+                "monitoreo.digital@avianca.com"
+            ];
+
+            const phoneNumbersData: Array<string> = [
+                "123456",
+                "987654",
+                "654321",
+                "321654",
+                "987123",
+                "456789",
+                "102938",
+                "112233",
+                "778899",
+                "334455"
+            ];
+
+            const getDataRandom = (data: Array<string> = []): string => {
+                return data[Math.floor(Math.random() * data.length)];
+            }
+
+            const getValueElement = (element: HTMLInputElement): string => {
+                let value: string | null = null;
+                if (element.name === "email") {
+                    console.log('----ENTRO-----');
+                    value = getDataRandom(emailsData);
+                }
+                else if (element.name === "confirmEmail") {
+                    value = getDataRandom(emailsData);
+                    console.log('----ENTRO-----');
+                }
+                else if (element.name === "phone_phoneNumberId") {
+                    value = getDataRandom(phoneNumbersData);
+                    console.log('----ENTRO-----');
+                }
+                else if (element.id.includes("IdFirstName")) {
+                    value = getDataRandom(userNamesData);
+                    console.log('----ENTRO-----');
+                }
+                else {
+                    value = getDataRandom(lastNamesData);
+                    console.log('----ENTRO-----lastNamesData' + lastNamesData);
+                }
+                return value;
+            }
+
+            const getButtonAndClickItem = () => {
+                const listOptions = document.querySelector(".ui-dropdown_list");
+                const buttonElement = listOptions?.querySelector(".ui-dropdown_item>button") as HTMLButtonElement;
+                buttonElement.click();
+            }
+
+            const setValuesDefaultAutoForm = async () => {
+                const elements = document.querySelectorAll('.ui-input');
+                Array.from(elements).forEach((element) => {
+                    if (element.tagName === "BUTTON") {
+                        const elementButton = element as HTMLButtonElement;
+                        elementButton.click();
+                        const listOptions = document.querySelector(".ui-dropdown_list");
+                        (listOptions?.querySelector(".ui-dropdown_item>button") as HTMLButtonElement)?.click();
+                        console.log('****element.id****' + element.id);
+                        if (element.id === "passengerId") {
+                            elementButton.click();
+                            setTimeout(() => {
+                                console.log('++++++++++Entro++++++++');
+                                getButtonAndClickItem();
+                            }, 1000);
+                        }
+                        else if (element.id === 'phone_prefixPhoneId') {
+                            setTimeout(() => {
+                                console.log('++++++++++Entro++++++++');
+                                elementButton.click();
+
+                                getButtonAndClickItem();
+                            }, 1000);
+                        }
+                        else {
+                            const cas = document.querySelector('#acceptNewCheckbox') as HTMLButtonElement;
+                            cas.click();
+                            elementButton.click();
+                            getButtonAndClickItem();
+                        }
+                    }
+                    else if (element.tagName === "INPUT") {
+                        const elementInput = element as HTMLInputElement;
+                        const containers = document.querySelectorAll(".ui-input-container");
+                        Array.from(containers).forEach(e => { e.classList.add("is-focused") });
+                        let eventBlur: Event = new Event("blur");
+                        let eventFocus: Event = new Event("focus");
+                        elementInput.value = getValueElement(elementInput);
+                        ['change', 'input'].forEach(event => {
+                            let handleEvent = new Event(event, { bubbles: true, cancelable: false });
+                            element.dispatchEvent(handleEvent);
+                        });
+                        element.dispatchEvent(eventFocus);
+                        setTimeout(() => {
+                            element.dispatchEvent(eventBlur);
+                            Array.from(containers).forEach(e => { e.classList.remove("is-focused") });
+                        }, 1000);
+                    }
+                });
+
+                await expect(page.locator('id=email')).toBeVisible();
+                await (page.locator('id=email')).fill('test@gmail.com');
+
+                await expect(page.locator('id=confirmEmail')).toBeVisible();
+                await (page.locator('id=confirmEmail')).fill('test@gmail.com');
+                //const fieldAuthoritation = document.querySelector("#acceptNewCheckbox") as HTMLInputElement;
+                // if (fieldAuthoritation) fieldAuthoritation.checked = true;
+
+                await page.waitForSelector("id=acceptNewCheckbox");
+                await expect(page.locator('id=acceptNewCheckbox')).toBeVisible();
+                await (page.locator('id=acceptNewCheckbox')).click()
+
+                await expect(page.locator('id=sendNewsLetter')).toBeVisible();
+                await (page.locator('id=sendNewsLetter')).click();
+            }
+            setValuesDefaultAutoForm();
+        });
+
+        const acceptCheckbox = page.locator('input#acceptNewCheckbox');
+        await expect(acceptCheckbox).toBeVisible();
+        await acceptCheckbox.check();
+
+        await takeScreenshot("llenado-de-pasajeros-ok");
+        await page.waitForTimeout(2000);
+        //boton de continuar para los servicios
+        await expect(page.locator(".button.page_button.btn-action").last()).toBeVisible();
+        await page.locator(".button.page_button.btn-action").last().click();
+
     });
 });
