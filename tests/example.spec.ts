a3cc090014f2589a1bcfcc1b8984a2606e318c90
Aviancaswat <aviancaswat@gmail.com>
2025-05-21 08:55:12 -0500

    New code playwright-extra v6

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -390,7 +390,6 @@ test.describe('Comenzo prueba avianca', () => {
         await takeScreenshot("llenado-de-pasajeros-ok");
         // Esperar a que aparezca el modal
         await page.waitForSelector('ngb-modal-window', { timeout: 5_000 });
-
         // Localizar el botón “OK” del footer y hacer click
         const okButton = page.locator('button.modal_footer_button-action', { hasText: 'OK' });
         await expect(okButton).toBeVisible();
